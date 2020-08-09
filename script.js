18df7d8041de0afe294b06fc61b485bf6d3c9561
brahmnoor <bsc73@drexel.edu>
2020-08-08 23:35:16 -0400

    website updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".typed").typed({
-		strings: ["Software Engineer", "Undergraduate", "Junior"],
+		strings: ["Software Engineer", "Undergraduate", "Senior"],
 		typeSpeed: 50,
 		startDelay: 1000,
 		backDelay: 2000,
